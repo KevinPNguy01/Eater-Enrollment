18416f2c80946e8dfd54e4834faeca84457e2bfd
Kevin Nguy <72185570+KevinPNguy01@users.noreply.github.com>
2024-06-18 22:22:34 -0700

    Fixed array typing

--- src/constants/types.ts
+++ src/constants/types.ts
@@ -6,10 +6,10 @@ export type SearchFields = {
 export type CourseOffering = {
     year: string
     quarter: string
-    instructors: [Instructor]
+    instructors: Instructor[]
     final_exam: string
     max_capacity: number
-    meetings: [Meeting]
+    meetings: Meeting[]
     num_section_enrolled: number
     num_total_enrolled: number
     num_new_only_reserved: number
@@ -29,25 +29,25 @@ export type Course = {
     school: string
     title: string
     course_level: string
-    department_alias: [string]
-    units: [number]
+    department_alias: string[]
+    units: number[]
     description: string
     department_name: string
-    instructor_history: [Instructor]
+    instructor_history: Instructor[]
     prerequisite_tree: string
-    prerequisite_list: [Course]
+    prerequisite_list: Course[]
     prerequisite_text: string
-    prerequisite_for: [Course]
+    prerequisite_for: Course[]
     repeatability: string
     concurrent: string
     same_as: string
     restriction: string
     overlap: string
     corequisite: string
-    ge_list: [string]
+    ge_list: string[]
     ge_text: string
-    terms: [string]
-    offerings: [CourseOffering]
+    terms: string[]
+    offerings: CourseOffering[]
 };
   
 export type Instructor = {
@@ -57,9 +57,9 @@ export type Instructor = {
     email: string
     title: string
     department: string
-    schools: [string]
-    related_departments: [string]
-    course_history: [Course]
+    schools: string[]
+    related_departments: string[]
+    course_history: Course[]
 };
   
 export type Meeting = {
