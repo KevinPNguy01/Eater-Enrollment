37c32591061d8a96677a7a9cec7e87eb246ff953
Kevin Nguy <72185570+KevinPNguy01@users.noreply.github.com>
2024-06-24 12:38:07 -0700

    Added grade distribution types

--- src/constants/types.ts
+++ src/constants/types.ts
@@ -74,3 +74,35 @@ export type SectionInfo = {
     number: string
     type: string
 };
+
+export type GradeDistributionCollection = {
+    aggregate: GradeDistributionCollectionAggregate
+    grade_distributions: GradeDistribution[]
+    instructors: string[]
+}
+
+export type GradeDistributionCollectionAggregate = {
+    sum_grade_a_count: number
+    sum_grade_b_count: number
+    sum_grade_c_count: number
+    sum_grade_d_count: number
+    sum_grade_f_count: number
+    sum_grade_p_count: number
+    sum_grade_np_count: number
+    sum_grade_w_count: number
+    average_gpa: number
+    count: number
+}
+
+export type GradeDistribution = {
+    grade_a_count: number
+    grade_b_count: number
+    grade_c_count: number
+    grade_d_count: number
+    grade_f_count: number
+    grade_p_count: number
+    grade_np_count: number
+    grade_w_count: number
+    average_gpa: number
+    course_offering: CourseOffering
+}
