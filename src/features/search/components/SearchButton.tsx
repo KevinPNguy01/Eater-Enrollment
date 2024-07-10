b549d2fd4a916a5e886313b3f4b983ae7d350523
Kevin Nguy <72185570+KevinPNguy01@users.noreply.github.com>
2024-07-10 00:01:32 -0700

    Fixed search button not working

--- src/features/search/components/SearchButton.tsx
+++ src/features/search/components/SearchButton.tsx
@@ -1,7 +1,7 @@
 export function SearchButton(props: {submit: () => void}) {
     return (
         <div className="flex w-full content-center justify-center">
-            <span className="bg-primary w-1/2 p-1 m-8 rounded text-center hover:cursor-pointer" onClick={props.submit}>
+            <span className="bg-primary w-1/2 p-1 m-8 rounded text-center hover:cursor-pointer" onClick={() => props.submit()}>
                 Search
             </span>
         </div>
