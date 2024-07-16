be206a890a64f1d0bda263f04002d40aab500597
Kevin Nguy <72185570+KevinPNguy01@users.noreply.github.com>
2024-07-16 16:33:58 -0700

    Combined year and quarter drop downs into one

--- src/app/pages/SearchForms.tsx
+++ src/app/pages/SearchForms.tsx
@@ -1,28 +1,23 @@
-import { DropDown } from "../../components/DropDown";
 import { SearchBox } from "../../features/search/components/SearchBox";
 import { SearchButton } from "../../features/search/components/SearchButton";
+import { TermDropDown } from "../../features/search/components/TermDropDown";
 import { Query } from "../../utils/PeterPortal";
 
 export function SearchForms(props: {queriesState: [Query[], (queries: Query[]) => void], defaultQueryState: [Query, (queries: Query) => void], submit: () => void}) {
     const {submit} = props;
     const [queries, setQueries] = props.queriesState;
     const [defaultQuery, setDefaultQuery] = props.defaultQueryState;
-
     const {quarter, year} = defaultQuery;
-    const setQuarter = (quarter: string) => setDefaultQuery({...defaultQuery, quarter});
-    const setYear = (year: string) => setDefaultQuery({...defaultQuery, year});
+    const setTerm = (quarter: string, year: string) => setDefaultQuery({quarter, year});
     
-    const termOptions = ["Fall", "Winter", "Spring", "Summer Session 1", "Summer Session 2", "10-wk Summer"];
-    const yearOptions = Array.from({ length: new Date().getFullYear() - 2015 + 2}, (_, index) => (new Date().getFullYear()+1 - index).toString());
     return (
         <form autoComplete="off" id="searchForm" className={`flex flex-col h-1 grow`} onSubmit={e => e.preventDefault()}>
-            <div className="grid grid-cols-2">
-                <DropDown label="Quarter" options={termOptions} default={quarter} setter={setQuarter}/>
-                <DropDown label="Year" options={yearOptions} default={year} setter={setYear}/>
+            <div className="">
+                <TermDropDown defaultTerm={{quarter, year}} setTerm={setTerm}/>
             </div>
             <br></br>
             <div className="relative flex-grow">
-                <SearchBox queriesState={[queries, setQueries]} defaultQuery={{quarter, year}} submit={submit}/>
+                <SearchBox queriesState={[queries, setQueries]} defaultQuery={defaultQuery} submit={submit}/>
                 <SearchButton submit={submit}/>
             </div>
         </form>
---
+++ src/features/search/components/TermDropDown.tsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+
+// All the possible quarter options.
+const quarterOptions: Record<string, string> = {
+    Fall: "Fall",
+    Winter: "Winter",
+    Spring: "Spring",
+    Summer1: "Summer Session 1",
+    Summer10wk: "10-week Summer",
+    Summer2: "Summer Session 2",
+}
+const quarterKeys = Object.keys(quarterOptions);
+
+// Current year and quarter
+const currentYear = new Date().getFullYear();
+const currentQuarter = "Fall";
+
+// Range of years from 2015-Current Year.
+const yearOptions = Array.from({ length: currentYear - 2015 + 1}, (_, index) => (currentYear - index - 1));
+
+// Determine quarters for current year.
+const endQuarterIndex = 1 + quarterKeys.findIndex(q => q === currentQuarter);
+const currentQuarters = quarterKeys.slice(0, endQuarterIndex);
+
+// Generate all quarters for past years.
+const pastQuarters = yearOptions.map(year => [year, quarterKeys] as [number, string[]]);
+
+// Group current and past quarters.
+const termGroups: [number, string[]][] = [[currentYear, currentQuarters], ...pastQuarters]
+
+export function TermDropDown(props: {defaultTerm: {quarter: string, year: string}, setTerm: (quarter: string, year: string) => void}) {
+    const {defaultTerm, setTerm} = props;
+
+    // React state hook for displaying the current term in the select box.
+    const [termLabel, setTermLabel] = useState(`${defaultTerm.quarter} ${defaultTerm.year}-${parseInt(defaultTerm.year) % 100 + 1}`);
+
+    return (
+        <div className="grid mx-1">
+            <label className="text-neutral-300 text-xs">Term</label>
+            <select 
+                className="bg-secondary" 
+                value="display"
+                onChange={({target}) => {
+                    const [quarter, year] = target.value.split(",");
+                    setTerm(quarter, (parseInt(year) + (quarter === "Fall" ? 0 : 1)).toString());
+                    setTermLabel(`${quarterOptions[quarter]} ${year}-${parseInt(year) % 100 + 1}`)
+                }}
+            >
+                {/** Hidden option used for displaying the current term. */}
+                <option hidden={true} value="display">{termLabel}</option>
+                {termGroups.map(([year, quarters]) => (
+                    <optgroup key={year} label={`${year}-${(year+1)%100}`}>
+                        {/** Group options by year. */}
+                        {quarters.map(quarter => (
+                            <option className={``} key={quarter} value={`${quarter},${year}`}>
+                                {quarterOptions[quarter]}
+                            </option>
+                        ))}
+                    </optgroup>
+                ))}
+            </select>
+        </div> 
+    )
+}
