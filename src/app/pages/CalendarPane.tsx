aa717e02cb0801f51bda2131ced2d303be7eefc3
Kevin Nguy <72185570+KevinPNguy01@users.noreply.github.com>
2024-09-16 23:20:43 -0700

    Optimize calendar for mobile/export as image

--- src/app/pages/CalendarPane.tsx
+++ src/app/pages/CalendarPane.tsx
@@ -7,6 +7,7 @@ import UndoIcon from '@mui/icons-material/Undo';
 import Backdrop from '@mui/material/Backdrop';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
+import ImageIcon from '@mui/icons-material/Image';
 import { CustomEventMenu } from 'features/calendar/components/CustomEventMenu';
 import { EventInfo } from 'features/calendar/components/EventInfo';
 import { ScheduleSelect } from 'features/calendar/components/ScheduleSelect';
@@ -18,6 +19,8 @@ import { clearSchedule } from 'stores/slices/ScheduleSet';
 import { createCustomEvents, createEvents, createFinalEvents } from 'utils/FullCalendar';
 import useWindowDimensions from 'utils/WindowDimensions';
 import { newCustomEvent } from '../../helpers/CustomEvent';
+import * as htmlToImage from 'html-to-image';
+import moment from 'moment';
 
 export function CalendarPane() {
 	const currentSchedule = useSelector(selectCurrentSchedule);
@@ -30,6 +33,7 @@ export function CalendarPane() {
 	const [customEvent, setCustomEvent] = useState(newCustomEvent());
 	const ref = useRef(null as unknown as HTMLDivElement);
 	const screenSize = useWindowDimensions();
+	const { height, width } = screenSize;
 
 	// Update calendar rect when screen size changes.
 	useEffect(() => {
@@ -48,6 +52,13 @@ export function CalendarPane() {
 			e => setScrollPos((e.target as HTMLDivElement).scrollTop));
 	}, []);
 
+	const isMobile = (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) || (width > height && 1.33 * width / 2 < height);
+	const showWeekend = showingFinals || currentSchedule.customEvents.map(({ days }) => days & 65).some(val => val);
+	const eventSize = isMobile ? (showWeekend ? "event-small event-smaller" : "event-small") : "";
+	const [contentHeight, setContentHeight] = useState("100%" as "100%" | "auto");
+	const [minTime, setMinTime] = useState("07:00");
+	const [maxTime, setMaxTime] = useState("22:00");
+
 	return (
 		<div
 			className={`flex flex-col flex-grow`}
@@ -65,21 +76,30 @@ export function CalendarPane() {
 					setCustomEvent(newCustomEvent());
 					setMenuState(bool)
 				}}
+				setContentHeight={setContentHeight}
+				setMinTime={setMinTime}
+				setMaxTime={setMaxTime}
 			/>
-			<div ref={ref} id="calendar" className="relative flex flex-col flex-grow relative">
+			<div ref={ref} id="calendar" className={`bg-secondary relative flex flex-col flex-grow relative ${eventSize}`}>
 				<FullCalendar
 					plugins={[timeGridPlugin]}
 					initialView="timeGridWeek"
 					headerToolbar={false}
-					weekends={showingFinals || currentSchedule.customEvents.map(({ days }) => days & 65).some(val => val)}
+					weekends={showWeekend}
 					allDaySlot={false}
-					height="100%"
+					height={contentHeight}
+					contentHeight={contentHeight}
 					expandRows={true}
-					slotMinTime="07:00:00"
-					slotMaxTime="23:00:00"
+					slotMinTime={minTime}
+					slotMaxTime={maxTime}
 					firstDay={showingFinals ? 6 : 7}
-					slotLabelFormat={{ hour: "numeric" }}
-					dayHeaderFormat={{ weekday: 'short' }}
+					slotLabelFormat={{
+						hour: "numeric",
+						meridiem: isMobile ? "short" : true
+					}}
+					dayHeaderFormat={{
+						weekday: 'short',
+					}}
 					eventBorderColor="#00000080"
 					eventClick={(info) => {
 						info.jsEvent.stopPropagation();
@@ -92,7 +112,7 @@ export function CalendarPane() {
 					eventTimeFormat={{
 						hour: "numeric",
 						minute: "2-digit",
-						meridiem: true
+						meridiem: !isMobile,
 					}}
 				/>
 				{/** Info card for clicked events. */}
@@ -118,17 +138,18 @@ export function CalendarPane() {
 
 }
 
-function CalendarNavBar(props: { showingFinals: boolean, setShowingFinals: (_: boolean) => void, menuState: boolean, setMenuState: (_: boolean) => void }) {
-	const { showingFinals, setShowingFinals, menuState, setMenuState } = props
+function CalendarNavBar(props: { showingFinals: boolean, setShowingFinals: (_: boolean) => void, menuState: boolean, setMenuState: (_: boolean) => void, setContentHeight: (_: "100%" | "auto") => void, setMinTime: (_: string) => void, setMaxTime: (_: string) => void }) {
+	const { showingFinals, setShowingFinals, menuState, setMenuState, setContentHeight, setMinTime, setMaxTime } = props
 	const prevState = useSelector(selectPastSchedules);
 	const nextState = useSelector(selectFutureSchedules);
 	const currentScheduleIndex = useSelector(selectCurrentScheduleIndex);
+	const currentSchedule = useSelector(selectCurrentSchedule);
 	const dispatch = useDispatch();
 
 	return (
-		<nav className="w-full bg-tertiary flex my-1 items-center justify-between px-0.5">
+		<nav className="w-full bg-tertiary flex flex-wrap my-1 items-center justify-between px-0.5">
 			{/** Container for components that change schedule view. */}
-			<div className="flex w-1/2 items-center gap-4 h-fit">
+			<div className="flex w-fit items-center gap-4 h-fit p-2">
 				<ScheduleSelect />
 				{/** Toggle finals button. */}
 				<Button
@@ -142,7 +163,55 @@ function CalendarNavBar(props: { showingFinals: boolean, setShowingFinals: (_: b
 				</Button>
 			</div>
 			{/** Container for buttons that mutate schedules. */}
-			<div className="px-4">
+			<div className="px-4 flex flex-nowrap">
+				{/** Download schedule image button. */}
+				<IconButton
+					color="white"
+					onClick={async () => {
+						const calendar = document.getElementById("calendar")!;
+						setContentHeight("auto");
+						const events = (
+							showingFinals ? createFinalEvents(currentSchedule.courses.map(({ offerings }) => offerings).flat()) :
+								createEvents(currentSchedule.courses.map(({ offerings }) => offerings).flat()).concat(createCustomEvents(currentSchedule.customEvents))
+						);
+						const times = events.map(event => {
+							const start = moment(event.start);
+							const end = moment(event.end);
+							const startTime = 60 * start.hours() + start.minutes();
+							const endTime = 60 * end.hours() + end.minutes();
+							return [startTime, endTime];
+						}).flat();
+						const start = moment("00:00", "HH:mm");
+						const end = moment("00:00", "HH:mm");
+						const minTime = Math.min(...times);
+						const maxTime = Math.max(...times);
+						const roundedMin = Math.floor((minTime - 1) / 60) * 60;
+						const roundedMax = Math.ceil((maxTime + 1) / 60) * 60;
+						start.set("minutes", roundedMin);
+						end.set("minutes", roundedMax);
+						if (minTime - roundedMin > 30) calendar.classList.add("clip-timegrid-start");
+						if (roundedMax - maxTime > 30) calendar.classList.add("clip-timegrid-end");
+						setMinTime(start.format("HH:mm"));
+						setMaxTime(end.format("HH:mm"));
+						await new Promise(r => setTimeout(r, 0));
+						htmlToImage.toPng(calendar as HTMLElement)
+							.then(function (dataUrl) {
+								const link = document.createElement('a');
+								link.href = dataUrl;
+								link.download = 'schedule.png';
+								const event = new MouseEvent('click');
+								link.dispatchEvent(event);
+
+								setContentHeight("100%");
+								setMinTime("07:00");
+								setMaxTime("22:00");
+								calendar.classList.remove("clip-timegrid-start");
+								calendar.classList.remove("clip-timegrid-end");
+							});
+					}}
+				>
+					<ImageIcon />
+				</IconButton>
 				{/** Undo button. */}
 				<IconButton
 					color="white"
