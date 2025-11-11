ed368e80077abbd59fe4bdd69c43115468676179
Kevin Nguy <72185570+KevinPNguy01@users.noreply.github.com>
2025-11-10 23:23:37 -0800

    chore: update default search term

--- src/features/search/components/TermDropDown.tsx
+++ src/features/search/components/TermDropDown.tsx
@@ -6,51 +6,63 @@ import { setQuarter, setYear } from "stores/slices/Search";
 
 // All the possible quarter options.
 const quarterOptions: Record<string, string> = {
-    Fall: "Fall",
-    Winter: "Winter",
-    Spring: "Spring",
-    Summer1: "Summer Session 1",
-    Summer10wk: "10-week Summer",
-    Summer2: "Summer Session 2",
-}
+  Fall: "Fall",
+  Winter: "Winter",
+  Spring: "Spring",
+  Summer1: "Summer Session 1",
+  Summer10wk: "10-week Summer",
+  Summer2: "Summer Session 2",
+};
 
 export function TermDropDown() {
-    const dispatch = useDispatch();
-    const year = useSelector(selectSearchYear);
-    const quarter = useSelector(selectSearchQuarter);
-    const { availableTerms } = useContext(AvailableTermsContext);
+  const dispatch = useDispatch();
+  const year = useSelector(selectSearchYear);
+  const quarter = useSelector(selectSearchQuarter);
+  console.log(quarter, year);
+  const { availableTerms } = useContext(AvailableTermsContext);
 
-    // React state hook for displaying the current term in the select box.
-    const termLabel = useMemo(() => `${quarterOptions[quarter]} ${year}-${parseInt(year) % 100 + 1}`, [quarter, year]);
+  // React state hook for displaying the current term in the select box.
+  const termLabel = useMemo(
+    () => `${quarterOptions[quarter]} ${year}-${(parseInt(year) % 100) + 1}`,
+    [quarter, year]
+  );
 
-    return (
-        <div className="grid mx-1">
-            <label className="text-neutral-300 text-xs">Term</label>
-            <select
-                className="bg-secondary"
-                value="display"
-                onChange={({ target }) => {
-                    const [quarter, year] = target.value.split(",");
-                    dispatch(setQuarter(quarter))
-                    dispatch(setYear(year));
-                }}
-            >
-                {/** Hidden option used for displaying the current term. */}
-                <option hidden={true} value="display">{termLabel}</option>
-                {Object.keys(availableTerms).toReversed().map(key => {
-                    const year = parseInt(key);
-                    return (
-                        <optgroup key={year} label={`${year}-${(year + 1) % 100}`}>
-                            {/** Group options by year. */}
-                            {availableTerms[key].map(([quarter, quarterString]) => (
-                                <option className={``} key={quarter} value={`${quarter},${year}`}>
-                                    {quarterString}
-                                </option>
-                            ))}
-                        </optgroup>
-                    )
-                })}
-            </select>
-        </div>
-    )
+  return (
+    <div className="grid mx-1">
+      <label className="text-neutral-300 text-xs">Term</label>
+      <select
+        className="bg-secondary"
+        value="display"
+        onChange={({ target }) => {
+          const [quarter, year] = target.value.split(",");
+          dispatch(setQuarter(quarter));
+          dispatch(setYear(year));
+        }}
+      >
+        {/** Hidden option used for displaying the current term. */}
+        <option hidden={true} value="display">
+          {termLabel}
+        </option>
+        {Object.keys(availableTerms)
+          .toReversed()
+          .map((key) => {
+            const year = parseInt(key);
+            return (
+              <optgroup key={year} label={`${year}-${(year + 1) % 100}`}>
+                {/** Group options by year. */}
+                {availableTerms[key].map(([quarter, quarterString]) => (
+                  <option
+                    className={``}
+                    key={quarter}
+                    value={`${quarter},${year}`}
+                  >
+                    {quarterString}
+                  </option>
+                ))}
+              </optgroup>
+            );
+          })}
+      </select>
+    </div>
+  );
 }
--- src/stores/slices/Search.ts
+++ src/stores/slices/Search.ts
@@ -4,78 +4,102 @@ import { Course } from "types/Course";
 import { ScheduleQuery } from "types/ScheduleQuery";
 
 export const searchSlice = createSlice({
-    name: "search",
-    initialState: {
-        quarter: "Spring",
-        year: "2024",
-        prevInput: "",
-        input: "",
-        prevQueries: [] as ScheduleQuery[],
-        queries: [] as ScheduleQuery[],
-        results: [] as Course[],
-        suggestions: [] as SearchSuggestion[],
-        prevType: "single" as "single" | "multi",
-        type: "single" as "single" | "multi",
-        display: false,
-        pending: false
+  name: "search",
+  initialState: {
+    quarter: "Winter",
+    year: "2025",
+    prevInput: "",
+    input: "",
+    prevQueries: [] as ScheduleQuery[],
+    queries: [] as ScheduleQuery[],
+    results: [] as Course[],
+    suggestions: [] as SearchSuggestion[],
+    prevType: "single" as "single" | "multi",
+    type: "single" as "single" | "multi",
+    display: false,
+    pending: false,
+  },
+  reducers: {
+    setResults: (state, action: PayloadAction<Course[]>) => {
+      state.results = action.payload;
     },
-    reducers: {
-        setResults: (state, action: PayloadAction<Course[]>) => {
-            state.results = action.payload;
-        },
-        setQuarter: (state, action: PayloadAction<string>) => {
-            state.quarter = action.payload;
-        },
-        setYear: (state, action: PayloadAction<string>) => {
-            state.year = action.payload;
-        },
-        addQuery: (state, action: PayloadAction<ScheduleQuery>) => {
-            state.queries.push(action.payload);
-            state.input = "";
-        },
-        removeQuery: (state, action: PayloadAction<number>) => {
-            state.queries.splice(action.payload, 1);
-        },
-        clearQueries: (state) => {
-            state.queries = [];
-        },
-        toggleSearchType: (state) => {
-            state.type = state.type === "single" ? "multi" : "single";
-        },
-        setDisplayResults: (state, action: PayloadAction<boolean>) => {
-            state.display = action.payload;
-        },
-        setSearchSuggestions: (state, action: PayloadAction<SearchSuggestion[]>) => {
-            state.suggestions = action.payload;
-        },
-        setSearchInput: (state, action: PayloadAction<string>) => {
-            state.input = action.payload;
-        },
-        setSearchType: (state, action: PayloadAction<"single" | "multi">) => {
-            state.type = action.payload;
-        },
-        setSearchPending: (state) => {
-            state.results = [];
-            state.display = true;
-            state.pending = true;
-        },
-        setSearchFulfilled: (state, action: PayloadAction<{ queries: ScheduleQuery[], courses: Course[], refresh: boolean }>) => {
-            const { queries, courses, refresh } = action.payload;
-            if (!refresh) {
-                state.prevQueries = queries;
-                state.prevInput = state.input;
-                state.prevType = state.type
-            } else {
-                state.input = state.prevInput;
-                state.queries = state.prevQueries;
-                state.type = state.prevType;
-            }
-            state.pending = false;
-            state.results = courses;
-        }
-    }
-})
+    setQuarter: (state, action: PayloadAction<string>) => {
+      state.quarter = action.payload;
+    },
+    setYear: (state, action: PayloadAction<string>) => {
+      state.year = action.payload;
+    },
+    addQuery: (state, action: PayloadAction<ScheduleQuery>) => {
+      state.queries.push(action.payload);
+      state.input = "";
+    },
+    removeQuery: (state, action: PayloadAction<number>) => {
+      state.queries.splice(action.payload, 1);
+    },
+    clearQueries: (state) => {
+      state.queries = [];
+    },
+    toggleSearchType: (state) => {
+      state.type = state.type === "single" ? "multi" : "single";
+    },
+    setDisplayResults: (state, action: PayloadAction<boolean>) => {
+      state.display = action.payload;
+    },
+    setSearchSuggestions: (
+      state,
+      action: PayloadAction<SearchSuggestion[]>
+    ) => {
+      state.suggestions = action.payload;
+    },
+    setSearchInput: (state, action: PayloadAction<string>) => {
+      state.input = action.payload;
+    },
+    setSearchType: (state, action: PayloadAction<"single" | "multi">) => {
+      state.type = action.payload;
+    },
+    setSearchPending: (state) => {
+      state.results = [];
+      state.display = true;
+      state.pending = true;
+    },
+    setSearchFulfilled: (
+      state,
+      action: PayloadAction<{
+        queries: ScheduleQuery[];
+        courses: Course[];
+        refresh: boolean;
+      }>
+    ) => {
+      const { queries, courses, refresh } = action.payload;
+      if (!refresh) {
+        state.prevQueries = queries;
+        state.prevInput = state.input;
+        state.prevType = state.type;
+      } else {
+        state.input = state.prevInput;
+        state.queries = state.prevQueries;
+        state.type = state.prevType;
+      }
+      state.pending = false;
+      state.results = courses;
+    },
+  },
+});
 
-export const { setSearchPending, setSearchFulfilled, setResults, setQuarter, setYear, addQuery, removeQuery, clearQueries, toggleSearchType, setDisplayResults, setSearchSuggestions, setSearchInput, setSearchType } = searchSlice.actions;
+export const {
+  setSearchPending,
+  setSearchFulfilled,
+  setResults,
+  setQuarter,
+  setYear,
+  addQuery,
+  removeQuery,
+  clearQueries,
+  toggleSearchType,
+  setDisplayResults,
+  setSearchSuggestions,
+  setSearchInput,
+  setSearchType,
+} = searchSlice.actions;
 
 export default searchSlice.reducer;
