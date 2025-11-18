16200332b9914d521c6bcf2dbf1992b5832e234e
Kevin Nguy <72185570+KevinPNguy01@users.noreply.github.com>
2025-11-17 20:17:03 -0800

    fix: on mobile, page defaults to calendar instead of search

--- src/app/pages/CoursesPane.tsx
+++ src/app/pages/CoursesPane.tsx
@@ -9,39 +9,48 @@ import { CalendarPane } from "./CalendarPane";
 import { navLinks } from "constants/Links";
 
 export function CoursesPane(props: { includeCalendar: boolean }) {
-    const { includeCalendar } = props;
-    const [activeTab, setActiveTab] = useState(includeCalendar ? "calendar" : "search");
-    const displaySearchResults = useSelector(selectDisplayResults);
+  const { includeCalendar } = props;
+  const [activeTab, setActiveTab] = useState(
+    includeCalendar ? "calendar" : "search"
+  );
+  const displaySearchResults = useSelector(selectDisplayResults);
 
-    // When switching to split screen, calendar tab won't exist in CoursesPane.
-    if (activeTab === "calendar" && !includeCalendar) {
-        setActiveTab("search");
-    }
+  // When switching to split screen, calendar tab won't exist in CoursesPane.
+  if (activeTab === "calendar" && !includeCalendar) {
+    setActiveTab("search");
+  }
 
-    return (
-        <div className="m-1 flex flex-col h-full gap-1">
-            <nav className="bg-tertiary h-8 md:h-12 grid grid-flow-col shrink-0">
-                {/** Generate nav buttons for Search, Added, Map, and Calendar if appropriate. */}
-                {navLinks.slice(includeCalendar ? 0 : 1).map(({ id, title }) => (
-                    <button
-                        key={id}
-                        className={`text-xs md:text-lg h-full ${id === activeTab ? "border-b-4 border-primary" : "text-neutral-300"}`}
-                        onClick={() => setActiveTab(id)}
-                    >
-                        {title}
-                    </button>
-
-                ))}
-            </nav>
-            {/** Return page components depending on the active tab. */}
-            {(function getActiveTab() {
-                switch (activeTab) {
-                    case "calendar": return <CalendarPane />;
-                    case "search": return displaySearchResults ? <SearchResults /> : <SearchForms />;
-                    case "added": return <AddedTab />;
-                    case "map": return <MapTab />;
-                }
-            })()}
-        </div>
-    )
+  return (
+    <div className="m-1 flex flex-col h-full gap-1">
+      <nav className="bg-tertiary h-8 md:h-12 grid grid-flow-col shrink-0">
+        {/** Generate nav buttons for Search, Added, Map, and Calendar if appropriate. */}
+        {navLinks.slice(includeCalendar ? 0 : 1).map(({ id, title }) => (
+          <button
+            key={id}
+            className={`text-xs md:text-lg h-full ${
+              id === activeTab
+                ? "border-b-4 border-primary"
+                : "text-neutral-300"
+            }`}
+            onClick={() => setActiveTab(id)}
+          >
+            {title}
+          </button>
+        ))}
+      </nav>
+      {/** Return page components depending on the active tab. */}
+      {(function getActiveTab() {
+        switch (activeTab) {
+          case "calendar":
+            return <CalendarPane />;
+          case "search":
+            return displaySearchResults ? <SearchResults /> : <SearchForms />;
+          case "added":
+            return <AddedTab />;
+          case "map":
+            return <MapTab />;
+        }
+      })()}
+    </div>
+  );
 }
